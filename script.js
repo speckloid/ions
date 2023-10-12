b767e9003950a08715a6827526711272a5fe2ab8
speckloid <147720488+speckloid@users.noreply.github.com>
2023-10-12 14:49:35 +0200

    Update script.js

--- script.js
+++ script.js
@@ -9,15 +9,15 @@ const canvasWidth = canvas.width;
 const canvasHeight = canvas.height;
 
 // Function to create an ion object
-function createIon(x, y, charge, mass) {
+function createIon(x, y, charge, mass, velocityX, velocityY) {
     return {
         x,
         y,
         charge,
         mass,
         radius: 20, // Adjust as needed
-        velocityX: 0, // Initial velocity in the x direction
-        velocityY: 0, // Initial velocity in the y direction
+        velocityX: velocityX || 0, // Initial velocity in the x direction
+        velocityY: velocityY || 0, // Initial velocity in the y direction
     };
 }
 
@@ -84,4 +84,21 @@ function updateIons() {
         if (ion.x - ion.radius < 0 || ion.x + ion.radius > canvasWidth) {
             ion.velocityX *= -1; // Reverse the x velocity
         }
-        if (ion.y - ion.radius < 0
+        if (ion.y - ion.radius < 0 || ion.y + ion.radius > canvasHeight) {
+            ion.velocityY *= -1; // Reverse the y velocity
+        }
+    });
+}
+
+// Add ions to the array
+ions.push(createIon(100, 100, 1, 1, 2, 2)); // Example: Positive ion with mass 1 and initial velocity
+ions.push(createIon(200, 200, -1, 1, -2, -2)); // Example: Negative ion with mass 1 and initial velocity
+
+function update() {
+    updateIons(); // Update ion positions based on velocity
+    ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+    drawIons(); // Draw ions in their updated positions
+    requestAnimationFrame(update);
+}
+
+update(); // Start the simulation loop
